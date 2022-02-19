c963c0751f677da90a54708d47e2a981ac6573c2
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-02-19 16:09:37 +0900

    [Silver III] Title: 01타일, Time: 108 ms, Memory: 15580 KB -BaekjoonHub

--- 백준/Silver/1904.01타일/01타일.java
+++ 백준/Silver/1904.01타일/01타일.java
@@ -5,17 +5,16 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int value = Integer.parseInt(br.readLine());
-        long[] arr = new long[1000001];
+        int[] arr = new int[1000001];
 
         arr[0] = 0;
         arr[1] = 1;
         arr[2] = 2;
 
         for (int i = 3; i <= value; i++) {
-            arr[i] = (arr[i-2] % 15746) + (arr[i-1] % 15746);
+            arr[i] = (arr[i-2] + arr[i-1]) % 15746;
         }
 
-        long answer = arr[value] % 15746L;
-        System.out.println(answer);
+        System.out.println(arr[value]);
     }
 }

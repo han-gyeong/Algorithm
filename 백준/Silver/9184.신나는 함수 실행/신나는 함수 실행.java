35c34f01a694aeeccbb91f78a374a404aa29fe83
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-02-19 14:01:34 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 516 ms, Memory: 45660 KB -BaekjoonHub

---
+++ 백준/Silver/9184.신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[][][] arr;
+    public static int w(int a, int b, int c) {
+        if (arr[a][b][c] > 0) {
+            return arr[a][b][c];
+        }
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return arr[a][b][c] = 1;
+        }
+
+        if (a > 20 || b > 20 || c > 20) {
+            return arr[a][b][c] = w(20, 20, 20);
+        }
+
+        if (a < b && b < c) {
+            return arr[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+        }
+
+        return arr[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        arr = new int[51][51][51];
+        StringBuffer sb = new StringBuffer();
+        while (true) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+            if (a == -1 && b == -1 && c == -1) {
+                break;
+            }
+            sb.append("w(" + a + ", " + b + ", " + c + ") = ");
+
+            if (a < 0) {
+                a = 0;
+            }
+
+            if (b < 0) {
+                b = 0;
+            }
+
+            if (c < 0) {
+                c = 0;
+            }
+
+            sb.append(w(a,b,c)).append('\n');
+        }
+        System.out.println(sb);
+    }
+}

86c33ccc70aa665cdb5f09fc958d2393610dee36
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-02-22 18:30:17 +0900

    [Silver I] Title: 정수 삼각형, Time: 784 ms, Memory: 115044 KB -BaekjoonHub

--- 백준/Silver/1932.정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932.정수 삼각형/정수 삼각형.java
@@ -2,36 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     static int[][] arr;
-    static int[][] dp;
+    static Integer[][] dp;
+    static int n;
 
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        arr = new int[n][];
-        dp = new int[n][];
+        n = scan.nextInt();
+        arr = new int[n][n];
+        dp = new Integer[n][n];
 
         for (int i = 0; i < n; i++) {
-            arr[i] = new int[i+1];
-            dp[i] = new int[i+1];
+            arr[i] = new int[i + 1];
             for (int j = 0; j <= i; j++) {
                 arr[i][j] = scan.nextInt();
             }
         }
 
-        dp[0][0] = arr[0][0];
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                dp[i + 1][j] = Math.max(dp[i+1][j], dp[i][j] + arr[i+1][j]);
-                dp[i + 1][j+1] = Math.max(dp[i+1][j+1], dp[i][j] + arr[i+1][j+1]);
-            }
+        for (int i = 0; i < arr[n-1].length; i++) {
+            dp[n-1][i] = arr[n-1][i];
         }
 
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            answer = Math.max(answer, dp[n-1][i]);
+        System.out.println(find(0, 0));
+    }
+
+    public static int find(int depth, int index) {
+        if (depth == n-1) {
+            return dp[n-1][index];
         }
 
-        System.out.println(answer);
+        if (dp[depth][index] == null) {
+            return dp[depth][index] = Math.max(find(depth + 1, index), find(depth + 1, index + 1)) + arr[depth][index];
+        }
+
+        return dp[depth][index];
     }
 }

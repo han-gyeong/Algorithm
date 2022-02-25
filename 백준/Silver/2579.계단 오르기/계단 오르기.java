31bf0e372e06033b0da6189e7f6f3bf8d0678954
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-02-25 20:57:34 +0900

    [Silver III] Title: 계단 오르기, Time: 128 ms, Memory: 13180 KB -BaekjoonHub

---
+++ 백준/Silver/2579.계단 오르기/계단 오르기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        dp[0] = arr[0] = 0;
+        dp[1] = arr[1];
+
+        if (n >= 2) {
+            dp[2] = arr[2] + arr[1];
+        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i];
+        }
+
+        System.out.println(dp[n]);
+    }
+}

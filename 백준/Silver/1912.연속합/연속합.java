4af6b72e49c8f3b721d66d531da403720db975bf
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-02-28 22:51:00 +0900

    [Silver II] Title: 연속합, Time: 392 ms, Memory: 28984 KB -BaekjoonHub

--- 백준/Silver/1912.연속합/연속합.java
+++ 백준/Silver/1912.연속합/연속합.java
@@ -1,17 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     static int[] arr, dp;
 
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        arr = new int[n];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         dp = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = scan.nextInt();
-        }
 
         dp[0] = arr[0];
         for (int i = 1; i < arr.length; i++) {

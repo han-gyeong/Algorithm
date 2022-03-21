b75675a61d3eead94c02290a6d4e747d9a543a09
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-03-21 21:53:37 +0900

    [Silver II] Title: 종이의 개수, Time: 1244 ms, Memory: 317052 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int n;
+    static int[][] arr;
+    static int[] answer;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][];
+        answer = new int[3];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        recursive(0, 0, n);
+
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
+        System.out.println(answer[2]);
+    }
+
+    public static void recursive(int x, int y, int size) {
+        if (isSameColor(x, y, size)) {
+            answer[arr[x][y] + 1]++;
+        } else {
+            for (int i = x; i < x + size; i += size / 3) {
+                for (int j = y; j < y + size; j += size / 3) {
+                    recursive(i, j, size / 3);
+                }
+            }
+        }
+    }
+
+    public static boolean isSameColor(int x, int y, int size) {
+        int color = arr[x][y];
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (arr[i][j] != color) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}

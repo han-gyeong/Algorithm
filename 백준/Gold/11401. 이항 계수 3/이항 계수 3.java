054be3443af7cff455a78578ad4245b961a18464
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-03-23 20:33:55 +0900

    [Gold I] Title: 이항 계수 3, Time: 160 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    static final int DIVISON = 1_000_000_007;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+
+
+        long foundAnswer = find(factorial(n - k) * factorial(k), DIVISON - 2);
+
+
+        long answer = (factorial(n) * foundAnswer) % DIVISON;
+        System.out.println(answer);
+    }
+
+    static long factorial(int n) {
+        long answer = 1;
+        while (n >= 1) {
+            answer = (answer * n) % DIVISON;
+            n--;
+        }
+
+        return answer;
+    }
+
+    static long find(long bottom, long up) {
+        if (up == 1) {
+            return bottom;
+        } else {
+            long tmpAnswer = find(bottom, up / 2) % DIVISON;
+            if (up % 2 == 1) {
+                return ((tmpAnswer * tmpAnswer % DIVISON) * (find(bottom, 1) % DIVISON)) % DIVISON;
+            } else {
+                return tmpAnswer * tmpAnswer % DIVISON;
+            }
+        }
+    }
+}

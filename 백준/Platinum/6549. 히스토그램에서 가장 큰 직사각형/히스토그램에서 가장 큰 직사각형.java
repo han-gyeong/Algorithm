d6458d47d233495a698e69e95f1ddbcf47f239cb
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-03-27 14:13:10 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 1116 ms, Memory: 238608 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
+    static int[] arr;
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
 
@@ -11,50 +11,59 @@ public class Main {
                 break;
             }
 
-            int[] arr = new int[n];
+            arr = new int[n];
             for (int i = 0; i < n; i++) {
                 arr[i] = scan.nextInt();
             }
 
-            System.out.println(solution(arr));
+            System.out.println(getArea(0, n-1));
         }
     }
 
-    public static long solution(int[] arr) {
-        Stack<Element> elements = new Stack<>();
-        elements.add(new Element(0, arr[0]));
-        long answer = 0;
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < elements.peek().value) {
-                int previous_index = arr.length + 1;
-                while (!elements.isEmpty() && arr[i] < elements.peek().value) {
-                    Element tmp = elements.pop();
-                    previous_index = Math.min(previous_index, tmp.from);
-                    answer = Math.max(answer, tmp.value + (long) tmp.value * ((i-1) - tmp.from));
-                }
-
-                elements.add(new Element(previous_index, arr[i]));
-            } else {
-                elements.add(new Element(i, arr[i]));
-            }
+    private static long getArea(int from, int to) {
+        if (from == to) {
+            return arr[from];
         }
 
-        while (!elements.isEmpty()) {
-            Element tmp = elements.pop();
-            answer = Math.max(answer, tmp.value + (long) ((arr.length-1) - tmp.from) * tmp.value);
+        int mid = (from + to) / 2;
+        long leftArea = getArea(from, mid);
+        long rightArea = getArea(mid + 1, to);
+
+        long answer = Math.max(leftArea, rightArea);
+
+        long midArea = getMidArea(from, mid, to);
+
+        return Math.max(answer, midArea);
+    }
+
+    private static long getMidArea(int from, int mid, int to) {
+        long answer = arr[mid];
+        int height = arr[mid];
+        int toLeft = mid, toRight = mid;
+
+        while (toLeft > from && toRight < to) {
+            if (arr[toLeft - 1] < arr[toRight + 1]) {
+                toRight++;
+                height = Math.min(height, arr[toRight]);
+            } else {
+                toLeft--;
+                height = Math.min(height, arr[toLeft]);
+            }
+            answer = Math.max(answer, (long) height * (toRight - toLeft + 1));
+        }
+
+        while (toRight < to) {
+            toRight++;
+            height = Math.min(height, arr[toRight]);
+            answer = Math.max(answer, (long) height * (toRight - toLeft + 1));
+        }
+
+        while (toLeft > from) {
+            toLeft--;
+            height = Math.min(height, arr[toLeft]);
+            answer = Math.max(answer, (long) height * (toRight - toLeft + 1));
         }
 
         return answer;
     }
-
-static class Element {
-    int from;
-    int value;
-
-    public Element(int from, int value) {
-        this.from = from;
-        this.value = value;
-    }
-}
 }

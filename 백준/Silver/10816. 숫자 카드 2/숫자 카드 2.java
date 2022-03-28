af1cdbccabe07673e3a1fce8e795b13c2df19eec
HANGYEONG RYU <71131071+han-gyeong@users.noreply.github.com>
2022-03-29 01:06:44 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1084 ms, Memory: 180292 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,21 +1,65 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int n = scan.nextInt();
-        int[] arr = new int[20_000_000 + 1];
 
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            arr[10_000_000 + scan.nextInt()]++;
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        n = scan.nextInt();
-        for (int i = 0; i < n; i++) {
-            sb.append(arr[10_000_000 + scan.nextInt()]).append(" ");
+        Arrays.sort(arr);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < m; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            sb.append((upper(arr, key) - lower(arr, key))).append(" ");
         }
 
         System.out.println(sb);
+    }
+
+    public static int lower(int[] arr, int target) {
+        int from = 0;
+        int to = arr.length;
+
+        while (from < to) {
+            int mid = (from + to) / 2;
+
+            if (target <= arr[mid]) {
+                to = mid;
+            } else {
+                from = mid + 1;
+            }
+        }
+
+        return from;
+    }
+
+    public static int upper(int[] arr, int target) {
+        int from = 0;
+        int to = arr.length;
+
+        while (from < to) {
+            int mid = (from + to) / 2;
+
+            if (target < arr[mid]) {
+                to = mid;
+            } else {
+                from = mid + 1;
+            }
+        }
+
+        return from;
     }
 }

0a84e1f48622b6849202bec13e2f53f6b5c35a84
HANGYEONG RYU <71131071+han-gyeong@users.noreply.github.com>
2022-04-02 20:23:13 +0900

    [Gold V] Title: 공유기 설치, Time: 992 ms, Memory: 163220 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int c = scan.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        long lt = 0;
+        long rt = arr[arr.length - 1] - arr[0] + 1;
+        while (lt < rt) {
+            long mid = (lt + rt) / 2;
+
+            int router = find(arr, mid);
+
+            if (router >= c) {
+                lt = mid + 1;
+            } else {
+                rt = mid;
+            }
+        }
+
+        System.out.println(lt - 1);
+    }
+
+    public static int find(int[] arr, long minLength) {
+        int lastHouse = arr[0];
+        int router = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - lastHouse >= minLength) {
+                router++;
+                lastHouse = arr[i];
+            }
+        }
+
+        return router;
+    }
+}

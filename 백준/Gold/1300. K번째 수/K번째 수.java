d113b0ae884983ddc70c5ee7097a72819dc4b609
HANGYEONG RYU <71131071+han-gyeong@users.noreply.github.com>
2022-04-04 13:21:57 +0900

    [Gold II] Title: K번째 수, Time: 132 ms, Memory: 11776 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,19 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int k = scan.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
 
         long lt = 1;
-        long rt = k;
-        
+        long rt = k; // X 값은 k를 넘지 않는다. B[k] = X;
+
         while (lt < rt) {
+            // X값
             long mid = (lt + rt) / 2;
 
+            // 앞에 몇개가 있는지 -> 인덱스
             long count = 0;
             for (int i = 1; i <= n; i++) {
+                // 나보다 작은게 몇개 있는지
                 count += Math.min(mid / i, n);
             }
 

92bc124fdf8a96af95ffa666722fe5b0bdf70ffa
HANGYEONG RYU <71131071+han-gyeong@users.noreply.github.com>
2022-04-06 18:03:10 +0900

    [Silver III] Title: 바이러스, Time: 80 ms, Memory: 11736 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,28 +1,39 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int[] ch;
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        int computer = scan.nextInt();
-        int line = scan.nextInt();
-        ch = new int[computer + 1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int computer = Integer.parseInt(br.readLine());
+        int line = Integer.parseInt(br.readLine());
+        int[] ch = new int[computer + 1];
 
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
         for (int i = 0; i <= computer; i++) {
             graph.add(new ArrayList<>());
         }
 
+        StringTokenizer st;
         for (int i = 0; i < line; i++) {
-            int from = scan.nextInt();
-            int to = scan.nextInt();
+            st = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
             graph.get(from).add(to);
             graph.get(to).add(from);
         }
 
-        ch[1] = 1;
-        DFS(1, 1, graph);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        while (!queue.isEmpty()) {
+            int n = queue.poll();
+            for (int x : graph.get(n)) {
+                if (ch[x] == 0) {
+                    ch[x] = 1;
+                    queue.add(x);
+                }
+            }
+        }
 
         int answer = 0;
         for (int i = 2; i < ch.length; i++) {
@@ -33,18 +44,4 @@ public class Main {
 
         System.out.println(answer);
     }
-
-    private static void DFS(int index, int before, ArrayList<ArrayList<Integer>> graph) {
-        if (index == ch.length) {
-            return;
-        } else {
-            for (int x : graph.get(before)) {
-                if (ch[x] == 0) {
-                    ch[x] = 1;
-                    DFS(index + 1, x, graph);
-                }
-            }
-        }
-    }
-
 }

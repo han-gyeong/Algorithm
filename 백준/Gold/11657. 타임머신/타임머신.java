2432e07ac8317dc93f4f93896750f6beae1bb915
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-04-18 16:23:16 +0900

    [Gold IV] Title: 타임머신, Time: 560 ms, Memory: 33000 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(); // 도시의 개수
+        int m = scan.nextInt(); // 노선 개수
+        long[] arr = new long[n+1];
+        Arrays.fill(arr, Integer.MAX_VALUE);
+        arr[1] = 0;
+
+        ArrayList<Road> roads = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            int from = scan.nextInt();
+            int to = scan.nextInt();
+            int price = scan.nextInt();
+            roads.add(new Road(from, to, price));
+        }
+
+        boolean isLoop = false;
+        for (int i = 0; i < n; i++) {
+            for (Road road : roads) {
+                if (arr[road.from] == Integer.MAX_VALUE) {
+                    continue;
+                }
+
+                if (arr[road.to] > arr[road.from] + road.price) {
+                    arr[road.to] = arr[road.from] + road.price;
+
+                    if (i == n-1) {
+                        isLoop = true;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (isLoop) {
+            sb.append(-1);
+        } else {
+            for (int i = 2; i <= n; i++) {
+                if (arr[i] == Integer.MAX_VALUE) {
+                    sb.append(-1);
+                } else {
+                    sb.append(arr[i]);
+                }
+                sb.append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static class Road {
+        int from;
+        int to;
+        int price;
+
+        public Road(int from, int to, int price) {
+            this.from = from;
+            this.to = to;
+            this.price = price;
+        }
+    }
+}

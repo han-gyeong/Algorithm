bea5cb4833acb5665adfd38e28f05ee4d018d657
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-05-14 00:30:01 +0900

    [Gold III] Title: 파일 합치기, Time: 500 ms, Memory: 25248 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,18 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         while (--n >= 0) {
-            int m = scan.nextInt();
-            int[] array = new int[m];
-
-            for (int i = 0; i < m; i++) {
-                array[i] = scan.nextInt();
-            }
+            int m = Integer.parseInt(br.readLine());
+            int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
             int[][] dp = new int[m][m];
             for (int i = 0; i < m; i++) {

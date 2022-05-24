ea510ebe06bc4c431261876309771641d3e1fa70
han-gyeong <71131071+han-gyeong@users.noreply.github.com>
2022-05-24 20:07:59 +0900

    [Gold III] Title: 파일 합치기, Time: 488 ms, Memory: 24972 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,29 +1,27 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        while (--n >= 0) {
+        while (n-- > 0) {
             int m = Integer.parseInt(br.readLine());
             int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
             int[][] dp = new int[m][m];
-            for (int i = 0; i < m; i++) {
-                Arrays.fill(dp[i], 0);
-            }
 
             for (int i = 1; i < m; i++) {
-                for (int j = i-1; j >= 0; j--) {
+                for (int j = i - 1; j >= 0; j--) {
                     int min = Integer.MAX_VALUE;
                     for (int k = 0; k < i - j; k++) {
                         min = Math.min(min, dp[j][j + k] + dp[j + k + 1][i]);
                     }
 
-                    dp[j][i] = min + sumRange(array, j, i);
+                    dp[j][i] = min + sumRange(j, i, array);
                 }
             }
 
@@ -31,12 +29,13 @@ public class Main {
         }
     }
 
-    public static int sumRange(int[] arr, int from, int to) {
-        int sum = 0;
+    public static int sumRange(int from, int to, int[] array) {
+        int answer = 0;
+
         for (int i = from; i <= to; i++) {
-            sum += arr[i];
+            answer += array[i];
         }
 
-        return sum;
+        return answer;
     }
 }
